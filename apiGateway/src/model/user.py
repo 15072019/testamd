e659867260f3ab69fcc567814492345b60107e8d
15072019 <the.vtop@gmail.com>
2025-02-26 21:04:16 +0700

    Done user part

--- apiGateway/src/model/user.py
+++ apiGateway/src/model/user.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__ = "User"
     
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    username = Column(String, index=True)
-    password = Column(String, index=True)
-    phone = Column(String,index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
