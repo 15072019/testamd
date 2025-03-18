d707e9f2b31be6817b2ef9e2f731a1612cf9c3ca
nhuutrong203 <nhuutrong102@gmail.com>
2025-03-18 14:10:38 +0700

    completed

--- services/ride_matching/ride_matching.py
+++ services/ride_matching/ride_matching.py
@@ -18,41 +18,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# producer = KafkaProducer(
-#     bootstrap_servers="localhost:9092",
-#     value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-# )
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
 
-# @app.get("/ride-matching/process/{booking_id}")
-# def process_ride_matching(booking_id: int):
-#     from src.web.ride_matching import get_ride_distance
-#     try:
-#         ride_data = get_ride_distance(booking_id)
-#          producer.send("ride-matching-topic", ride_data)
-#         return {"message": "Ride matching processed", "data": ride_data}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+@app.get("/ride-matching/process/{booking_id}")
+def process_ride_matching(booking_id: int):
+    from src.web.ride_matching import get_ride_distance
+    try:
+        ride_data = get_ride_distance(booking_id)
+        producer.send("ride-matching-topic", ride_data)
+        return {"message": "Ride matching processed", "data": ride_data}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-# @app.get("/ride-matching/events")
-# def get_ride_matching_events():
-#     try:
-#          consumer = KafkaConsumer(
-#              "ride-matching-topic",
-#              bootstrap_servers="localhost:9092",
-#              value_deserializer=lambda m: json.loads(m.decode("utf-8")),
-#              auto_offset_reset="earliest",
-#              enable_auto_commit=True,
-#          )
+@app.get("/ride-matching/events")
+def get_ride_matching_events():
+    try:
+        consumer = KafkaConsumer(
+            "ride-matching-topic",
+            bootstrap_servers="localhost:9092",
+            value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+            auto_offset_reset="earliest",
+            enable_auto_commit=True,
+        )
 
-#         events = []
-#          for message in consumer:
-#              events.append(message.value)
-#              if len(events) >= 6:
-#                  break
+        events = []
+        for message in consumer:
+            events.append(message.value)
+            if len(events) >= 6:
+                break
 
-#         return {"events": events}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+        return {"events": events}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     uvicorn.run("services.ride_matching:app", reload=True, host="0.0.0.0", port=8004)
